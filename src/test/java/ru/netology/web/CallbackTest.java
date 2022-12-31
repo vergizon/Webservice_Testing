e6cb7fb74bcecdd167bfdc07f8dc294ad0f8243b
Alex Vergizov <alvergizoff@gmail.com>
2022-12-31 18:38:53 +0300

    test corrected

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -23,7 +23,12 @@ public class CallbackTest {
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
